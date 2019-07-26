a4877001e54657003602cfacef52fd392a15cac8
Maison <maisongalvao144@gmail.com>
2019-07-26 13:26:18 -0400

    Aula06 - Desafios

--- aula06/imc.php
+++ aula06/imc.php
@@ -1,14 +1,13 @@
 <?php
-$peso = 75.4;
+$peso = 76.5;
 $altura = 1.74;
 
 $imc = $peso / ($altura ** 2);
-if($imc <18.5){
+if($imc <18)
     echo "Abaixo do peso";
-} else if ($imc >=18.5 and $imc <=24.9){
+ else if ($imc >18 and $imc <25)
     echo "Peso Normal";
-} else if ($imc >= 25.0 and $imc <=29.9){
+ else if ($imc >= 25 and $imc <=29)
     echo "Sobrepeso";
-}else {
+else if ($imc > 29)
     echo "Obesidade grau 1";
-}
